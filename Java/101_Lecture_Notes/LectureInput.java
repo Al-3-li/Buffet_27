9f2c9d1ebaaceb96ce797f30fdf3c358960ce59e
Al-3-li <a.sk.3.ip.m@gmail.com>
2026-09-15 17:57:17 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,29 @@
 /*
     Lecture note example - Input!!
 */
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+        System.out.println("Willkommen zum Deutsch Essen!");
+       System.out.println("Unsere Spiesekarte:");
+        System.out.print("1. Schnitzel - ");
+        Double Eins = 2.5;
+        System.out.println(Eins);
+
+        System.out.print("2. Kaesebrot - ");
+        Double zwei = 4.5;
+        System.out.println(zwei);
+
+        System.out.print("3. Currywurst - ");
+        Double drei = 5.6;
+        System.out.println(drei);
+
+        System.out.println("Wie viele Schnitzel moechten Sie?");
+        Scanner sc = new Scanner(System.in);
+
+        int item1Quant = sc.nextInt();
+        System.out.println("Schnitzel kosten:" Eins*item1Quant);
+
 	}
 }
